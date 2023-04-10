bf6e041193bbedf9ad0f21745e69cad38ee7a3e3
Tong Tang <pichip22@hotmail.com>
2023-04-09 20:18:16 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById(`main`);
+main.remove();
+
+const newHeader = document.createElement(`h1`);
+
+newHeader.id = `victory`;
+
+newHeader.textContent = `Tong is the champion`
